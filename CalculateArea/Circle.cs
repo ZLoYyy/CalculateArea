0e2421e10f62c6ea61cc1bdbbf1b91b3bb848e47
Zloy <qzloyp.gmail.com>
2022-06-30 01:18:39 +0900

    Добавлено сокращение значения до сотых

--- CalculateArea/Circle.cs
+++ CalculateArea/Circle.cs
@@ -5,10 +5,16 @@ using System.Text;
 
 namespace CalculateArea
 {
-    public class Circle: IFigure
+    public class Circle : IFigure
     {
+        /// <summary>
+        /// Радиус окружности
+        /// </summary>
         public double Radius { get; }
-
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="radius"></param>
         public Circle(double radius)
         {
             if (radius < 0 )
@@ -22,7 +28,7 @@ namespace CalculateArea
         public double GetArea()
         {
             double s = Math.PI * Math.Pow(Radius, 2);
-
+            s = Math.Round(s, 2);
             return s;
         }
     }
