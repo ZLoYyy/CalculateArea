faa428d4927e142bd56371dc8fbe07ccc0b0491c
Zloy <qzloyp.gmail.com>
2022-06-30 01:20:00 +0900

    Добавлены тесты на валидацию

--- CalculateArea.Tests/CalculateAreaTests.cs
+++ CalculateArea.Tests/CalculateAreaTests.cs
@@ -1,10 +1,19 @@
 using NUnit.Framework;
+using System;
 
 namespace CalculateArea.Tests
 {
     [TestFixture]
     public class Tests
     {
+        /// <summary>
+        /// Circle validation
+        /// </summary>
+        [Test]
+        public void CircleValidationTest()
+        {
+            Assert.Throws<ArgumentException>(()=> new Circle(-1));
+        }        
         /// <summary>
         /// Calculate Circle area 
         /// </summary>
@@ -15,7 +24,18 @@ namespace CalculateArea.Tests
 
             var area = circle.GetArea();
 
-            Assert.AreEqual(615.7521601035994, area);
+            Assert.AreEqual(615.75, area);
+        }
+        /// <summary>
+        /// Triangle validation
+        /// </summary>
+        [Test]
+        public void TriangleValidationTest()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(10, 24, -15));
+            Assert.Throws<ArgumentException>(() => new Triangle(10, -24, 15));
+            Assert.Throws<ArgumentException>(() => new Triangle(-10, 24, -15));
+            Assert.Throws<ArgumentException>(() => new Triangle(9, 12, 23));
         }
         /// <summary>
         /// Calculate trinagle area 
@@ -27,7 +47,7 @@ namespace CalculateArea.Tests
 
             var area = triangle.GetArea();
 
-            Assert.AreEqual(41.07843108006926, area);
+            Assert.AreEqual(41.08, area);
         }
         /// <summary>
         /// Trinagle is rectangular
@@ -37,7 +57,7 @@ namespace CalculateArea.Tests
         {
             Triangle triangle = new Triangle(3, 4, 5);
 
-            var area = triangle.IsRectangular;
+            var area = triangle.IsRectangularCheck();
 
             Assert.AreEqual(true, area);
         }
@@ -49,21 +69,44 @@ namespace CalculateArea.Tests
         {
             Triangle triangle = new Triangle(10, 24, 15);
 
-            var area = triangle.IsRectangular;
+            var area = triangle.IsRectangularCheck();
 
             Assert.AreEqual(false, area);
         }
         /// <summary>
-        /// Calculating area without knowing the figure type
+        /// Figure Validation
         /// </summary>
         [Test]
-        public void FigureCalculateAreaTest()
+        public void FigureValidationTest()
+        {
+            Assert.Throws<ArgumentException>(() => new Figure(new Circle(-9)));
+            Assert.Throws<ArgumentException>(() => new Figure(new Triangle(10, 24, -15)));
+            Assert.Throws<ArgumentException>(() => new Figure(new Triangle(10, -24, 15)));
+            Assert.Throws<ArgumentException>(() => new Figure(new Triangle(-10, 24, 15)));
+            Assert.Throws<ArgumentException>(() => new Figure(new Triangle(9, 12, 23)));
+        }
+        /// <summary>
+        /// Calculating area without knowing the figure type (Circle)
+        /// </summary>
+        [Test]
+        public void FigureCircleCalculateAreaTest()
         {
             Figure figure= new Figure(new Circle(9));
 
             var area = figure.GetArea();
 
-            Assert.AreEqual(254.46900494077323, area);
+            Assert.AreEqual(254.47, area);
+        }
+        /// <summary>
+        /// Calculating area without knowing the figure type (Triangle)
+        /// </summary>
+        [Test]
+        public void FigureTriangleCalculateAreaTest()
+        {
+            Figure figure = new Figure(new Triangle(13, 12, 23));
+            var area = figure.GetArea();
+
+            Assert.AreEqual(56.28, area);
         }
     }
 }
